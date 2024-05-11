a6886cfcb74da17c0657c6d19c73f451fc96acc5
Abhishek Sharma <sharmaabhishek9929@gmail.com>
2024-05-11 12:18:53 +0530

    some change on index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -63,20 +63,3 @@ ConnectDB()
     console.log('Error Occurs: ', err);
   });
 
-
-// ------------- Deployment ------------
-/*
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname1, '/frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'));
-  })
-} else {
-  app.get('/', (req, res => {
-    res.send('API is running successfully');
-  }))
-  
-}
-*/
-// ------------- Deployment ------------
